f920794c8881abebc9da6435e97aa8069b164c7d
Coleene <acevedocoleene@gmail.com>
2025-03-08 17:49:57 +0100

    created react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import React from "react";
+import Portfolio from "./Portfolio";
+function App() {
+  return (
+    <div className='App'>
+      <Portfolio />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Portfolio.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Portfolio.css";
+
+export default function Portfolio() {
+  return (
+    <div className='Portfolio'>
+      <div className='container'>
+        <h2>Hello there!</h2>
+      </div>
+    </div>
+  );
+}
