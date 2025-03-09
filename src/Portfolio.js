bd9d9ad8e6ba02895e39a5369034d4cd0833c90a
Coleene <acevedocoleene@gmail.com>
2025-03-09 15:22:26 +0100

    created hero and added css

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./Portfolio.css";
-
 export default function Portfolio() {
   return (
     <div className='Portfolio'>
-      <div className='container'>
-        <h2>Hello there!</h2>
+      <div className='hero d-flex justify-content-evenly'>
+        <section>
+          <h2>
+            Hi! I'm <em> Coleene Acevedo</em>
+          </h2>
+          <p>
+            I'm a Junior Front-End Developer based in Lausanne, Switzerland.
+          </p>
+        </section>
+        <div>
+          <img src='/portrait.jpg' alt='img-portrait' className='img-fluid  ' />
+        </div>
       </div>
     </div>
   );
