b2bab97e5aeaf936bc77b0537f220fc255627537
Coleene <acevedocoleene@gmail.com>
2025-03-09 16:46:19 +0100

    Added motion

---
+++ src/Biography.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Biography.css";
+export default function Biography(props) {
+  return (
+    <div className='Biography'>
+      <h3>BIOGRAPHY</h3>
+      <p>
+        I am currently working as a Sous-Chef and with my spare time I have
+        studied and accomplished a coding bootcamp from SheCodes.
+      </p>
+    </div>
+  );
+}
--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,21 +1,22 @@
 import React from "react";
 import "./Portfolio.css";
+import { motion } from "framer-motion";
+import Biography from "./Biography";
 export default function Portfolio() {
   return (
     <div className='Portfolio'>
       <div className='hero d-flex justify-content-evenly'>
-        <section>
+        <motion.section animate={{ y: 100 }}>
           <h2>
             Hi! I'm <em> Coleene Acevedo</em>
           </h2>
-          <p>
-            I'm a Junior Front-End Developer based in Lausanne, Switzerland.
-          </p>
-        </section>
-        <div>
+          <p>A Junior Front-End Developer based in Lausanne, Switzerland 🇨🇭.</p>
+        </motion.section>
+        <motion.div whileHover={{ scale: 1.2 }}>
           <img src='/portrait.jpg' alt='img-portrait' className='img-fluid  ' />
-        </div>
+        </motion.div>
       </div>
+      <Biography />
     </div>
   );
 }
